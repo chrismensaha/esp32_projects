3afc60c6a371c715e53bacf86d3d95ef55bfd462
chrismensaha <chrisanin76563@gmail.com>
2026-02-16 17:47:15 -0500

    Replace with RTOS task

--- projects/led_blink/src/main.cpp
+++ projects/led_blink/src/main.cpp
@@ -9,17 +9,20 @@ bool external_state=0;
 void setup(){
   pinMode(internal_led,OUTPUT);
   pinMode(external_led,OUTPUT); 
+  xTaskCreatePinnedToCore();
   
+}
+
+void toggleInternalLed(void* parameter){
+  while(1){
+    internal_state=!internal_state;
+    digitalWrite(internal_led,internal_state);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+  }
 }
 
 void loop() {
   unsigned long current_time=millis();
-  if (current_time-internal_previous_time>=500){
-    internal_state=!internal_state;
-    digitalWrite(internal_led,internal_state);
-    internal_previous_time+500;
-  }
-
   if (current_time-external_previous_time>=1000){
     external_state=!external_state;
     digitalWrite(external_led,external_state);
