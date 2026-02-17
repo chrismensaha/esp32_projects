85836f1e4703c09da70735511e07888fa7390f86
chrismensaha <chrisanin76563@gmail.com>
2026-02-16 21:06:25 -0500

    Move state to each toggle

--- projects/led_blink/src/main.cpp
+++ projects/led_blink/src/main.cpp
@@ -3,22 +3,23 @@ static constexpr uint8_t internal_led = 2;
 static constexpr uint8_t external_led = 4;
 unsigned long internal_previous_time=0;
 unsigned long external_previous_time=0;
-bool internal_state=0;
-bool external_state=0;
+
 
 
 void toggleInternalLed(void* parameter){
   while(1){
-    internal_state=!internal_state;
-    digitalWrite(internal_led,internal_state);
+    bool state=0;
+    state=!state;
+    digitalWrite(internal_led,state);
     vTaskDelay(500/portTICK_PERIOD_MS);
   }
 }
 
 void toggleExternalLed(void* parameter){
   while(1){
-    external_state=!external_state;
-    digitalWrite(external_led,external_state);
+    bool state=0;
+    state=!state;
+    digitalWrite(external_led,state);
     vTaskDelay(500/portTICK_PERIOD_MS);
   }
 }
@@ -26,8 +27,8 @@ void toggleExternalLed(void* parameter){
 void setup(){
   pinMode(internal_led,OUTPUT);
   pinMode(external_led,OUTPUT); 
-  xTaskCreatePinnedToCore(toggleInternalLed, "Internal LED Task",1024,NULL,1,NULL,0);
-  xTaskCreatePinnedToCore(toggleExternalLed, "EXternal LED Task",1024,NULL,1,NULL,1);
+  xTaskCreatePinnedToCore(toggleInternalLed, "Internal_Task",1024,NULL,1,NULL,0);
+  xTaskCreatePinnedToCore(toggleExternalLed, "EXternal_Task",1024,NULL,1,NULL,1);
 }
 
 void loop() {
