779bacbcf232a2bfd681f36d3be176e48e4332c4
chrismensaha <chrisanin76563@gmail.com>
2026-02-17 15:34:03 -0500

    Replace queue receive logic

--- projects/led_blink/src/main.cpp
+++ projects/led_blink/src/main.cpp
@@ -2,7 +2,6 @@
 static constexpr uint8_t internal_led = 2;
 static constexpr uint8_t external_led = 4;
 
-
 QueueHandle_t led_queue;
 
 void toggleInternalLed(void* parameter){
@@ -11,27 +10,24 @@ void toggleInternalLed(void* parameter){
   while(1){
     state=!state;
     digitalWrite(internal_led,state);
-   
     if (!state){
       counter++;
     }
     if (counter>=5){
-       int msg=1; 
+       uint8_t msg=1; 
       xQueueSend(led_queue,&msg,portMAX_DELAY); 
       counter=0;
     }
-    vTaskDelay(500/portTICK_PERIOD_MS);  
+    vTaskDelay(500/portTICK_PERIOD_MS);
     }
-    
   }
-
 
 void toggleExternalLed(void* parameter){
   int received_msg=0;
   bool state=false;
   while(1){
     if (xQueueReceive(led_queue,&received_msg,portMAX_DELAY)==pdPASS){
-      for (int i=0;i<6;i++){
+      for (uint8_t i=0;i<(received_msg*2);i++){
         state=!state;
         digitalWrite(external_led,state);
         vTaskDelay(100/portTICK_PERIOD_MS);
@@ -41,15 +37,13 @@ void toggleExternalLed(void* parameter){
 }
 
 void setup(){
-  led_queue=xQueueCreate(10,sizeof(int));
+  led_queue=xQueueCreate(10,sizeof(uint8_t));
   pinMode(internal_led,OUTPUT);
-  pinMode(external_led,OUTPUT); 
+  pinMode(external_led,OUTPUT);
   xTaskCreatePinnedToCore(toggleInternalLed, "Internal_Task",1024,NULL,1,NULL,0);
   xTaskCreatePinnedToCore(toggleExternalLed, "EXternal_Task",1024,NULL,1,NULL,1);
 }
 
-void loop() {
-}
 
-
+void loop(){}
 
