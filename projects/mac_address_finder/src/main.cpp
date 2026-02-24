5ef79ea18fd07e0bc5129d4fcd840acfee3b0192
chrismensaha <chrisanin76563@gmail.com>
2026-02-23 20:44:41 -0500

    Refactor MAC address retrieval to use esp_read_mac and improve task management

--- projects/mac_address_finder/src/main.cpp
+++ projects/mac_address_finder/src/main.cpp
@@ -1,19 +1,31 @@
 #include <Arduino.h>
-#include <WiFi.h>
+#include "esp_mac.h"
 
-void wifiTask(void* parameter){
-    WiFi.mode(WIFI_STA);
-    for(;;){
-        Serial.print("ESP32 MAC Address: ");
-        Serial.println(WiFi.macAddress());
-        vTaskDelay(5000/portTICK_PERIOD_MS);
+#define SERIAL_BAUD          115200
+#define SERIAL_INIT_TIMEOUT  3000
+#define REPORT_INTERVAL      5000  
+#define TASK_STACK_SIZE      2048
+#define TASK_PRIORITY        1
+#define TARGET_CORE          0
+
+void identityTask(void *parameter){
+    uint8_t mac[6];
+    for (;;){
+        if (esp_read_mac(mac,ESP_MAC_WIFI_STA)==ESP_OK){
+            Serial.printf("MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);            
+            UBaseType_t high_water_mark=uxTaskGetStackHighWaterMark(NULL);
+            Serial.printf("Stacks: %u bytes\n",(unsigned int)high_water_mark);
+        }
+        vTaskDelay(pdMS_TO_TICKS(REPORT_INTERVAL));
     }
 }
 
-void setup(){
-    Serial.begin(115200);
-    xTaskCreatePinnedToCore(wifiTask,"Wifi_Task",2048,NULL,1,NULL,0);
+void setup() {
+    Serial.begin(SERIAL_BAUD);   
+    uint32_t start_attempt=millis();
+    while (!Serial && (millis()-start_attempt<SERIAL_INIT_TIMEOUT));  
+    xTaskCreatePinnedToCore(identityTask,"ID_Task",TASK_STACK_SIZE,NULL,TASK_PRIORITY,NULL,TARGET_CORE);
+    vTaskDelete(NULL);
 }
-
 
 void loop(){}
