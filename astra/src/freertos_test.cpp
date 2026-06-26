e9bc203434741c94c9f6983ac9f31c2b63760d31
chrismensaha <chrisanin76563@gmail.com>
2026-06-26 14:59:20 -0400

    Refactor task and queue management for improved clarity and add mutex for serial output synchronization

--- astra/src/freertos_test.cpp
+++ astra/src/freertos_test.cpp
@@ -3,9 +3,11 @@
 
 enum{btn_UP,btn_DOWN,btn_ATK};
 
-TaskHandle_t task1=NULL;
-QueueHandle_t msg_QUEUE=NULL;
-QueueHandle_t btn_QUEUE=NULL;
+TaskHandle_t indicatorTask=NULL;
+QueueHandle_t msgQUEUE=NULL;
+QueueHandle_t btnQUEUE=NULL;
+SemaphoreHandle_t serialMutex=NULL;
+
 
 void vIndicatorLight(void *pvParameter){
     uint8_t pin=(uint32_t)pvParameter;
@@ -19,7 +21,7 @@ void vIndicatorLight(void *pvParameter){
 void vSendMessage(void* pvParameter){
     char* msg=(char*)pvParameter;
     for(;;){
-        if (xQueueSend(msg_QUEUE,&msg,pdMS_TO_TICKS(1000))==pdPASS){};
+        if (xQueueSend(msgQUEUE,&msg,pdMS_TO_TICKS(1000))==pdPASS){};
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 };
@@ -27,8 +29,10 @@ void vSendMessage(void* pvParameter){
 void vReceiveMessage(void* pvParameter){
     char* rec_msg=NULL;
     for(;;){
-        if (xQueueReceive(msg_QUEUE,&rec_msg,portMAX_DELAY)==pdPASS){
+        if (xQueueReceive(msgQUEUE,&rec_msg,portMAX_DELAY)==pdPASS){
+            xSemaphoreTake(serialMutex,portMAX_DELAY);
             Serial.println(rec_msg);
+            xSemaphoreGive(serialMutex);
         }   
         vTaskDelay(pdMS_TO_TICKS(100));
     }
@@ -37,7 +41,7 @@ void vReceiveMessage(void* pvParameter){
 void vButtonPressed(void* pvParameter){
     uint8_t button=(uint32_t)pvParameter;
     for(;;){
-        if (xQueueSend(btn_QUEUE,&button,pdMS_TO_TICKS(100))==pdPASS){};
+        if (xQueueSend(btnQUEUE,&button,pdMS_TO_TICKS(100))==pdPASS){};
         vTaskDelay(pdMS_TO_TICKS(100));
     }   
 }
@@ -45,22 +49,27 @@ void vButtonPressed(void* pvParameter){
 void vButtonReleased(void* pvParameter){
     uint8_t receive_BTN=0;
     for (;;){
-    if (xQueueReceive(btn_QUEUE,&receive_BTN,portMAX_DELAY)==pdPASS){
+    if (xQueueReceive(btnQUEUE,&receive_BTN,portMAX_DELAY)==pdPASS){
+        xSemaphoreTake(serialMutex,portMAX_DELAY);
         Serial.println("Button Pressed!");
+        xSemaphoreGive(serialMutex);
     }
     vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-void setup(){
+void setup(){ 
+    if (serialMutex==NULL) serialMutex=xSemaphoreCreateMutex();
+    
     Serial.begin(115200);
-    if (msg_QUEUE==NULL) msg_QUEUE=xQueueCreate(10,sizeof(char*));
-    if (btn_QUEUE==NULL) btn_QUEUE=xQueueCreate(10,sizeof(uint8_t));
-    if(task1==NULL) xTaskCreate(vIndicatorLight,"IndicatorTask",1024,(void*)pin2,1,&task1);
+    if (msgQUEUE==NULL) msgQUEUE=xQueueCreate(10,sizeof(char*));
+    if (btnQUEUE==NULL) btnQUEUE=xQueueCreate(10,sizeof(uint8_t));
+    if(indicatorTask==NULL) xTaskCreate(vIndicatorLight,"IndicatorTask",1024,(void*)pin2,1,&indicatorTask);
     xTaskCreate(vSendMessage,"SendTask",1024,(void*)"Hello",1,NULL);
     xTaskCreate(vReceiveMessage,"ReceiveTask",2048,NULL,1,NULL);
     xTaskCreate(vButtonPressed,"ButtonPressed",1024,(void*)btn_UP,1,NULL);
     xTaskCreate(vButtonReleased,"ButtonReleased",2048,NULL,1,NULL);
+
 }
 
 void loop(){};
