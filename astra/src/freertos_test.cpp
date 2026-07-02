cba3bbb349458fefa17ac9b63ba79a3dfb3839d4
chrismensaha <chrisanin76563@gmail.com>
2026-07-01 21:04:08 -0400

    Refactor button handling to use task notifications instead of queues and improve ISR implementation

--- astra/src/freertos_test.cpp
+++ astra/src/freertos_test.cpp
@@ -6,18 +6,17 @@ enum{btn_UP,btn_DOWN,btn_ATK};
 
 static TaskHandle_t indicatorTask=NULL;
 static QueueHandle_t msgQUEUE=NULL;
-static QueueHandle_t btnQUEUE=NULL;
+static TaskHandle_t buttonTask=NULL;
 static SemaphoreHandle_t serialMUTEX=NULL;
 
 
 static void vIndicatorLight(void *pvParameter){
     uint8_t pin=(uint32_t)pvParameter;
     for(;;){
-        digitalWrite(pin, !digitalRead(pin));
+        digitalWrite(pin,!digitalRead(pin));
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
-
  
 static void vSendMessage(void* pvParameter){
     char* msg=(char*)pvParameter;
@@ -43,16 +42,16 @@ static void vButtonISR(void){
     BaseType_t pxHigherPriorityTaskWoken=pdFALSE;
     uint8_t pinState=digitalRead(btnPIN);
     uint8_t buttonEvent=(pinState==LOW) ? 1 : 0;
-    xQueueSendFromISR(btnQUEUE,&buttonEvent,&pxHigherPriorityTaskWoken);
+    xTaskNotifyFromISR(buttonTask,buttonEvent,eSetValueWithOverwrite,&pxHigherPriorityTaskWoken);
     if (pxHigherPriorityTaskWoken){
         portYIELD_FROM_ISR();
     }
 }
 
 static void vButtonReleased(void* pvParameter){
-    uint8_t serialCheck=0;   
+    uint32_t serialCheck=0;   
     for (;;){
-        if (xQueueReceive(btnQUEUE,&serialCheck,portMAX_DELAY)==pdPASS){
+        if (xTaskNotifyWait(0x00,0xFFFFFFFF,&serialCheck,portMAX_DELAY)==pdPASS){
             if (xSemaphoreTake(serialMUTEX,pdMS_TO_TICKS(10))==pdPASS){
                 if (serialCheck==1){
                     Serial.println("Button Pressed!");
@@ -74,11 +73,10 @@ void setup(){
     Serial.begin(115200);
     if (serialMUTEX==NULL) serialMUTEX=xSemaphoreCreateMutex();   
     if (msgQUEUE==NULL) msgQUEUE=xQueueCreate(10,sizeof(char*));
-    if (btnQUEUE==NULL) btnQUEUE=xQueueCreate(10,sizeof(uint8_t));
     if(indicatorTask==NULL) xTaskCreate(vIndicatorLight,"IndicatorTask",1024,(void*)PIN2,1,&indicatorTask);
     xTaskCreate(vSendMessage,"SendTask",1024,(void*)"Hello",1,NULL);
     xTaskCreate(vReceiveMessage,"ReceiveTask",2048,NULL,1,NULL);
-    xTaskCreate(vButtonReleased,"Button Released",2048,NULL,1,NULL);
+    xTaskCreate(vButtonReleased,"Button Released",2048,NULL,1,&buttonTask);
     attachInterrupt(digitalPinToInterrupt(btnPIN),vButtonISR,CHANGE);
 }
 
